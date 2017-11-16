0dcfc9957bf14e625035210e102f360973149786
Dan Cutting <dan@cutting.io>
2017-11-16 15:21:56 +0000

    Initial Commit

---
+++ RemixMirror/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemixMirror
+//
+//  Created by Dan Cutting on 16/11/2017.
+//  Copyright © 2017 Schibsted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
