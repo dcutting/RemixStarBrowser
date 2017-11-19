95aea0071b12d9cc3c6c1b313a67ea0c590c4996
Dan Cutting <dan@cutting.io>
2017-11-19 21:10:52 +0000

    ID not needed for tests.

--- RemixMirror/StarBrowser/Fixtures/GivenTheWebServiceReturnsTheseStars.swift
+++ RemixMirror/StarBrowser/Fixtures/GivenTheWebServiceReturnsTheseStars.swift
@@ -3,18 +3,16 @@ var stubbedStars = [Star]()
 @objc(GivenTheWebServiceReturnsTheseStars)
 class GivenTheWebServiceReturnsTheseStars: NSObject {
 
-    @objc var id = ""
     @objc var name = ""
     @objc var summary = ""
 
     @objc func reset() {
-        id = ""
         name = ""
         summary = ""
     }
 
     @objc func execute() {
-        let star = Star(id: Star.ID(value: id), name: name, summary: summary)
+        let star = Star(id: Star.ID.make(), name: name, summary: summary)
         stubbedStars.append(star)
     }
 }
