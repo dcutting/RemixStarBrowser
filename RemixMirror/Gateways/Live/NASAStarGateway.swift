d9e21adfc13c2ef0a0e31f55e62a791dd4a244ff
Dan Cutting <dan@cutting.io>
2017-11-19 21:40:15 +0000

    Fix name.

--- RemixMirror/Gateways/Live/NASAStarGateway.swift
+++ RemixMirror/Gateways/Live/NASAStarGateway.swift
@@ -1,4 +1,4 @@
-class LiveStarGateway: StarGateway {
+class NASAStarGateway: StarGateway {
 
     func loadAll(completion: @escaping (Result<[Star]>) -> Void) {
         // TODO: connect to the real NASA API
