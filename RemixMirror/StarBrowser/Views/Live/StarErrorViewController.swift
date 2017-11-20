3cdde0cee8f71215abdda73d6536ed1b00780572
Dan Cutting <dan@cutting.io>
2017-11-20 11:26:25 +0000

    Live error view.

--- RemixMirror/StarBrowser/Views/Live/StarErrorViewController.swift
+++ RemixMirror/StarBrowser/Views/Live/StarErrorViewController.swift
@@ -1,4 +1,13 @@
 import Layout
 
 class StarErrorViewController: LayoutViewController, StarErrorView {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        loadLayout(named: "StarErrorView.xml")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
