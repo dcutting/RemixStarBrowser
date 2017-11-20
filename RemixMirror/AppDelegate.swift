04a8ca4c7fe1bce1af880cab2ddd71ae46288d58
Dan Cutting <dan@cutting.io>
2017-11-20 12:51:57 +0000

    Simplify app launch.

--- RemixMirror/AppDelegate.swift
+++ RemixMirror/AppDelegate.swift
@@ -3,32 +3,38 @@ import UIKit
 @UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var rootFlow: RootFlow?
+    var rootFlow: StarBrowserFlow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        let window = UIWindow(frame: UIScreen.main.bounds)
-
         let navigationController = UINavigationControllerWireframe()
+        rootFlow = makeStarBrowserFlow(navigationWireframe: navigationController)
+        configureWindow(rootViewController: navigationController)
+        rootFlow?.start()
+
+        return true
+    }
+
+    private func makeStarBrowserFlow(navigationWireframe: NavigationWireframe) -> StarBrowserFlow {
+
         let viewFactory = StarBrowserViewControllerFactory()
         let stars = [
             Star(id: Star.ID.make(), name: "Sirius", summary: "Sirius is a star system and the brightest star in the Earth's night sky."),
             Star(id: Star.ID.make(), name: "Canopus", summary: "Canopus is the brightest star in the southern constellation of Carina."),
             Star(id: Star.ID.make(), name: "Arcturus", summary: "Together with Spica and Denebola, Arcturus is part of the Spring Triangle asterism.")
         ]
-        let gateway = StarGatewayStub(.success(stars))
-        let deps = RootFlow.Dependencies(navigationWireframe: navigationController,
-                                         starBrowserViewFactory: viewFactory,
-                                         starGateway: gateway)
-        let rootFlow = RootFlow(deps: deps)
+        let gateway = StarGatewayStub(.success(stars))  // Use NASAStarGateway in real app.
 
-        window.rootViewController = navigationController
+        let deps = StarBrowserFlow.Dependencies(navigationWireframe: navigationWireframe,
+                                                starBrowserViewFactory: viewFactory,
+                                                starGateway: gateway)
+        return StarBrowserFlow(deps: deps)
+    }
+
+    private func configureWindow(rootViewController: UIViewController) {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootViewController
         window.makeKeyAndVisible()
         self.window = window
-        self.rootFlow = rootFlow
-
-        rootFlow.start()
-
-        return true
     }
 }
