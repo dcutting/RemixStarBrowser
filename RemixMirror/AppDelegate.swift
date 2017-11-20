22bfd50de3d6fd3ab92d4a529b4f8b77c6aa6684
Dan Cutting <dan@cutting.io>
2017-11-20 13:31:25 +0000

    Tidy up.

--- RemixMirror/AppDelegate.swift
+++ RemixMirror/AppDelegate.swift
@@ -17,18 +17,24 @@ import UIKit
 
     private func makeStarBrowserFlow(navigationWireframe: NavigationWireframe) -> StarBrowserFlow {
 
-        let viewFactory = StarBrowserViewControllerFactory()
-        let stars = [
+        let deps = StarBrowserFlow.Dependencies(navigationWireframe: navigationWireframe,
+                                                starBrowserViewFactory: StarBrowserViewControllerFactory(),
+                                                starGateway: makeGateway())
+        return StarBrowserFlow(deps: deps)
+    }
+
+    private func makeGateway() -> StarGateway {
+        let gateway = StarGatewayStub()  // Use NASAStarGateway in real app.
+        gateway.behaviour = .success(makeStars())
+        return gateway
+    }
+
+    private func makeStars() -> [Star] {
+        return [
             Star(id: Star.ID.make(), name: "Sirius", summary: "Sirius is a star system and the brightest star in the Earth's night sky."),
             Star(id: Star.ID.make(), name: "Canopus", summary: "Canopus is the brightest star in the southern constellation of Carina."),
             Star(id: Star.ID.make(), name: "Arcturus", summary: "Together with Spica and Denebola, Arcturus is part of the Spring Triangle asterism.")
         ]
-        let gateway = StarGatewayStub(.success(stars))  // Use NASAStarGateway in real app.
-
-        let deps = StarBrowserFlow.Dependencies(navigationWireframe: navigationWireframe,
-                                                starBrowserViewFactory: viewFactory,
-                                                starGateway: gateway)
-        return StarBrowserFlow(deps: deps)
     }
 
     private func configureWindow(rootViewController: UIViewController) {
