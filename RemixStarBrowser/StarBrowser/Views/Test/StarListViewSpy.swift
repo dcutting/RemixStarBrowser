e02db0f0d2e9290250963a9853a7f11768c24059
Dan Cutting <dan@cutting.io>
2017-11-29 12:47:12 +0000

    Clarify spy helper code.

--- RemixStarBrowser/StarBrowser/Views/Test/StarListViewSpy.swift
+++ RemixStarBrowser/StarBrowser/Views/Test/StarListViewSpy.swift
@@ -1,23 +1,25 @@
 import Foundation
 
 class StarListViewSpy: StarListView {
-    
     var viewData = StarListViewData.empty
     var delegate: StarListViewDelegate?
+}
+
+extension StarListViewSpy {
+
+    func selectAnyRow() {
+        select(row: 0)
+    }
 
     func select(row: NSNumber?) {
         guard let row = row?.intValue else { return }
         select(row: row)
     }
 
-    func select(row: Int) {
+    private func select(row: Int) {
         guard (0..<viewData.entries.count).contains(row) else { return }
         let star = viewData.entries[row]
         delegate?.didSelectStar(withID: star.id)
-    }
-
-    func selectAnyRow() {
-        select(row: 0)
     }
 }
 
